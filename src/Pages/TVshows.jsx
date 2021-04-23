58d51537cadb30c3efcf2d7bb15b58a7f3c13336
Emanuel <emanueltejada31295@gmail.com>
2021-04-23 13:58:12 -0400

    Add tv shows section data, call api

--- src/Pages/TVshows.jsx
+++ src/Pages/TVshows.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React , {useState, useEffect}  from 'react'
+import Resource from '../components/Resource/Resource'
+
+
+const featured_tvShows = 'https://api.themoviedb.org/3/tv/popular?api_key=7b2fdaac902fafe3262222fbf37450b7'
 
 const TVshows = () => {
-    return (
-        <div className='container'>
-            Tv Shows section
-        </div>
+    const [tvShows, setTvShows] = useState([])
+
+        useEffect(() => {
+          fetch(featured_tvShows)
+          .then( res => res.json())
+          .then(data => {
+            setTvShows(data.results)
+            console.log(data.results)
+          })
+
+          // eslint-disable-next-line
+        }, [])
+
+        return (
+        <>
+          <div className='container resource-container'>
+             {tvShows.length > 0? tvShows.map((resource) => (
+                <Resource {...resource} key={resource.id} />
+             )): 'No Tv Shows  found'}
+          </div>      
+        </>
     )
 }
 
--- src/components/Resource/Resource.jsx
+++ src/components/Resource/Resource.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 const image_api = 'https://image.tmdb.org/t/p/w1280/'
 
 
-const Resource = ({title, overview,release_date, poster_path, vote_average, adult}) => {
+const Resource = ({title, overview,release_date, poster_path, vote_average, name, first_air_date, adult}) => {
     return (
         <div className='resource'>
             <img src={`${image_api}${poster_path}`} alt="resource poster"/>
             <div className="resource-info">
-                 <h4>{title}</h4>
+                 <h4>{title || name}</h4>
                 <span>{vote_average}</span>   
             </div>
             <div className='aditional-info'>
                 <div className='top'>
-                    <p>{release_date}</p>
+                    <p>{release_date || first_air_date}</p>
                     <p>{adult? '+18' : '+13'}</p> 
                 </div>
                 <p>{overview}</p>

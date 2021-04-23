8218e861509c2684de2516d8ade71894f68c18b9
Emanuel <emanueltejada31295@gmail.com>
2021-04-23 13:13:57 -0400

    fix some responsive bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,11 +7,14 @@ const Navbar = () => {
     return (
         <>   
             <nav className='container navbar'>
-                <Link to='/' className='logo'><h1>Fakeflix</h1></Link> 
-                <ul className='nav-links'>
-                    <li ><Link to='/movies' className='nav-link'>Movies</Link></li>
-                    <li ><Link to='/tvshows'className='nav-link'>Tv Shows</Link></li>
-                </ul>
+                <div className="wrapper">
+                    <Link to='/' className='logo'><h1>Fakeflix</h1></Link> 
+                    <ul className='nav-links'>
+                        <li ><Link to='/movies' className='nav-link'>Movies</Link></li>
+                        <li ><Link to='/tvshows'className='nav-link'>Tv Shows</Link></li>
+                    </ul>
+                </div>
+                    
                  <input type="search" placeholder='Search...' className='search-box'/>
             </nav>
 
